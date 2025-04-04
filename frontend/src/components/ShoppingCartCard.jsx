08d301778037ce41eff24cf1427ebfc29ebad918
Kiara BBaldo <zbaldk@bbcag.ch>
2025-04-04 15:26:24 +0200

    ShoppingCartCard finished

--- frontend/src/components/ShoppingCartCard.jsx
+++ frontend/src/components/ShoppingCartCard.jsx
@@ -1,18 +1,41 @@
 import IndexRoute from "@/routes/IndexRoute";
 import styles from "./ShoppingCartCard.module.css"
 import "./ShoppingCartCard.module.css"
+import { useState } from "react";
+import Button from "./Button";
 
 
-export default function ShoppingCartCard({product}){
-  const {id, name, marke, volumes } = product
+export default function ShoppingCartCard({ product }) {
+  const { id, name, marke, volumes } = product
+  const [ count, setCount] = useState(1)
+
+  const increment = () => {
+    setCount(count+1)
+  }
+
+  const decrement = () => {
+    setCount(count-1)
+  }
+
+  function calcPrice(price, count){
+    let result = price*count
+    let result2 = Math.round(result * 100)/100
+    return result2.toFixed(2)
+  }
 
   return (
     <article >
       <img src="/snacks.png" alt="Snacks" className={styles.drinksSnacks} />
-          <h3>{name}</h3>
-          <p>{marke}</p>
-          <p>{`${volumes[1].volume}`}</p>
-          <p>{`${volumes[1].price} chf`}</p>
+      <div>
+        <h3>{name}</h3>
+        <p>{marke}</p>
+      </div>
+      <div>
+        <Button onClick={decrement}>-</Button>
+        {count}
+        <Button onClick={increment}>+</Button>
+        <p>{calcPrice(volumes[1].price, count)} CHF.</p>
+      </div>
     </article>
   );
 }
