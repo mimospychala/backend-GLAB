d2ab95fa836f6c152e41e3818af8c2e6ed804668
Flurin Klaey <zklaef@bbcag.ch>
2025-04-04 11:10:21 +0200

    made componets site to create components and edit them

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css"
 import App from "./App.jsx"
 import { createBrowserRouter, RouterProvider } from "react-router"
 import IndexRoute from "@/routes/IndexRoute.jsx"
+import ComponetRoute from "./routes/ComponetRoute"
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <IndexRoute/>
+      },
+      {
+        path: "componets",
+        element: <ComponetRoute/>
       }
     ]
   }
@@ -23,3 +28,5 @@ createRoot(document.getElementById("root")).render(
       <RouterProvider router={router}/>
     </StrictMode>
 )
+
+
---
+++ frontend/src/routes/ComponetRoute.jsx
@@ -0,0 +1,76 @@
+import SideBar from "@/components/SideBar"
+import LinkButton from "@/components/LinkButton"
+import ShoppingCartCard from "@/components/ShoppingCartCard"
+
+export default function ComponetRoute() {
+    return (
+        <>
+            <SideBar />
+            <LinkButton to="/kasse">Kasse</LinkButton>
+            <ShoppingCartCard product={product[1]} />
+        </>
+    )
+}
+
+const product = [
+    {
+        "id": "1",
+        "name": "Cola",
+        "marke": "Coca-Cola",
+        "volumes": [
+            {
+                "volume": "0.5L",
+                "price": "1.50"
+            },
+            {
+                "volume": "1.0L",
+                "price": "2.50"
+            }
+        ]
+    },
+    {
+        "id": "2",
+        "name": "Fanta",
+        "marke": "Coca-Cola",
+        "volumes": [
+            {
+                "volume": "0.5L",
+                "price": "1.40"
+            },
+            {
+                "volume": "1.0L",
+                "price": "2.40"
+            }
+        ]
+    },
+    {
+        "id": "3",
+        "name": "Snickers",
+        "marke": "Mars",
+        "volumes": [
+            {
+                "volume": "50g",
+                "price": "1.20"
+            },
+            {
+                "volume": "100g",
+                "price": "2.00"
+            }
+        ]
+    },
+    {
+        "id": "4",
+        "name": "Chips",
+        "marke": "Pringles",
+        "volumes": [
+            {
+                "volume": "150g",
+                "price": "2.50"
+            },
+            {
+                "volume": "200g",
+                "price": "3.50"
+            }
+        ]
+    }
+]
