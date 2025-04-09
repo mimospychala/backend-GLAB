9e56ddc36fcb2e981186aebc690eae270ccf10d0
Flurin Klaey <zklaef@bbcag.ch>
2025-04-09 09:36:21 +0200

    SideBar functions completed

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,14 +3,12 @@ import "./SideBar.module.css"
 import { Children, useState } from "react";
 import SideBarItem from "./SideBarItem";
 
-export default function SideBar({ sideBarItems }) {
+export default function SideBar({sideBarItems}) {
 
     return (
         <>
-            <div className={styles.sideBar}>
-                <h2>Side Bar</h2>
-                {sideBarItems.map((item) => <SideBarItem title={item.name} items={item.children} />)}
-            </div>
+            <h2>Side Bar</h2>
+            {sideBarItems.map((item) => <SideBarItem title={item.name} items={item.children}/>)}
         </>
     )
 }
