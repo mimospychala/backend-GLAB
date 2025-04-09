89be262c2230cf8b1edade69d48ba284eef84c6f
Flurin Klaey <zklaef@bbcag.ch>
2025-04-09 09:36:21 +0200

    SideBar functions completed

--- frontend/src/components/SideBarItem.jsx
+++ frontend/src/components/SideBarItem.jsx
@@ -1,23 +1,22 @@
+import { Link } from "react-router";
 import styles from "./SideBarItem.module.css"
 import { useState } from "react"
 
-export default function SideBarItem({ title, products }){
+export default function SideBarItem({ title, items = "", url = ""}) {   
     const [isOpen, setIsOpen] = useState(false);
-
-    return(
-        
+    return (
         <div>
-            <h3 onClick={() => setIsOpen(!isOpen)}>{title}</h3>
+            {url ? <Link><h3>{title}</h3></Link> : <h3 onClick={() => setIsOpen(!isOpen)}>{title}</h3>}
             {isOpen && (
-               
-               <ul>
-               {products.map((product, index) => (
-                   <li key={index}>
-                       <a href="#">{product}</a>
-                   </li>
-               ))}
-           </ul>
-       )}
-   </div>
-);
+                <ul>
+                    {items && items.map((item) => (
+                            <li>
+                            <SideBarItem title={item.name} items={item.children} url={item.url} />
+                            </li>
+                            ))   
+                    }
+                </ul>
+            )}
+        </div>
+    );
 }
