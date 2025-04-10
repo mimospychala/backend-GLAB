c24aa37362e7113ef8e7b9078da2045b5c09ac49
Flurin Klaey <zklaef@bbcag.ch>
2025-04-10 15:32:11 +0200

    updated button logic

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,13 +1,12 @@
 import LinkButton from "./LinkButton";
 import styles from "./Button.module.css"
 import "./Button.module.css"
-import clsx from "clsx";
 
-export default function Button({ to = "", children, className, ...props }) {
-    const combinedClass = clsx(styles.button, className);
+export default function Button({ to = "", children, className="", ...props }) {
+    const combinedClass = `${styles.button} ${className} `.trim();
     return(
         <>
-            {to ? <LinkButton to={to} className={combinedClass} {...props}>{children}</LinkButton> : <button className={styles.button} {...props}>{children}</button>}
+            {to ? <LinkButton to={to} className={combinedClass} {...props}>{children}</LinkButton> : <button className={combinedClass} {...props}>{children}</button>}
         </>
     )
 }
