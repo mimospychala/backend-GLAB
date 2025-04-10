7f3bda3cf7a920ccee98f204defcb8697c332b31
Kiara BBaldo <zbaldk@bbcag.ch>
2025-04-10 14:20:36 +0200

    merge

--- frontend/src/routes/IndexRoute.jsx
+++ frontend/src/routes/IndexRoute.jsx
@@ -2,11 +2,13 @@ import Header from "@/components/Header"
 import LinkButton from "@/components/LinkButton"
 import SideBar from "@/components/SideBar"
 import ShoppingCartCard from "@/components/ShoppingCartCard"
+import ProductCard from "@/components/ProductCard"
 
 export default function IndexRoute() {
   return (
     <>
-      
+      <ProductCard product={product[0]} />
+
     </>
-    )
+  )
 }
