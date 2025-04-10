2944e9acad1d06459e7db358cfe8899d875aa538
Flurin Klaey <zklaef@bbcag.ch>
2025-04-10 13:57:05 +0200

    Change projekt with mimo

--- backend/src/main/java/ch/bbcag/backend/DatabaseSeed.java
+++ backend/src/main/java/ch/bbcag/backend/DatabaseSeed.java
@@ -5,45 +5,169 @@ import ch.bbcag.backend.account.AccountRepository;
 import ch.bbcag.backend.account.AccountService;
 import ch.bbcag.backend.categorie.Categorie;
 import ch.bbcag.backend.categorie.CategorieService;
+import ch.bbcag.backend.combo.Combo;
+import ch.bbcag.backend.combo.ComboService;
+import ch.bbcag.backend.comment.Comment;
+import ch.bbcag.backend.comment.CommentService;
 import ch.bbcag.backend.price.Price;
 import ch.bbcag.backend.price.PriceService;
+import ch.bbcag.backend.product.Product;
 import ch.bbcag.backend.product.ProductRepository;
+import ch.bbcag.backend.product.ProductService;
 import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Arrays;
 
 @Component
 public class DatabaseSeed implements CommandLineRunner {
     private final AccountService accountService;
     private final CategorieService categorieService;
     private final PriceService priceService;
-    private final ProductRepository productRepository;
+    private final ComboService comboService;
+    private final CommentService commentService;
+    private final ProductService productService;
 
-    public DatabaseSeed(AccountService accountService, CategorieService categorieService, PriceService priceService, ProductRepository productRepository) {
+    public DatabaseSeed(AccountService accountService, CategorieService categorieService, PriceService priceService, ComboService comboService, ProductService productService, CommentService commentService) {
         this.categorieService = categorieService;
         this.accountService = accountService;
         this.priceService = priceService;
-        this.productRepository = productRepository;
+        this.comboService = comboService;
+        this.productService = productService;
+        this.commentService = commentService;
     }
 
     @Transactional
     @Override
     public void run(String... args) throws Exception {
-        Account account = new Account();
-        account.setEmail("admin@bbcag.com");
-        account.setPassword("admin1234");
-        account.setUsername("Admin");
-        accountService.create(account);
+        Account admin = new Account();
+        admin.setEmail("admin@bbcag.com");
+        admin.setPassword("admin1234");
+        admin.setUsername("Admin");
+        accountService.create(admin);
 
-        Categorie categorie = new Categorie();
-        categorie.setName("Burger");
-        categorieService.insert(categorie);
+        // Kategorien
+        Categorie drinkCat = new Categorie();
+        drinkCat.setName("Drinks");
+        categorieService.insert(drinkCat);
 
-        Price price = new Price();
-        price.setVolume("0.5l");
-        price.setPrice(BigDecimal.valueOf(1.53));
-        priceService.insert(price);
+        Categorie snackCat = new Categorie();
+        snackCat.setName("Snacks");
+        categorieService.insert(snackCat);
+
+        // Preise
+        Price small = new Price();
+        small.setVolume("0.33l");
+        small.setPrice(BigDecimal.valueOf(1.20));
+        priceService.insert(small);
+
+        Price medium = new Price();
+        medium.setVolume("0.5l");
+        medium.setPrice(BigDecimal.valueOf(1.50));
+        priceService.insert(medium);
+
+        Price large = new Price();
+        large.setVolume("1.0l");
+        large.setPrice(BigDecimal.valueOf(2.20));
+        priceService.insert(large);
+
+        List<Product> products = new ArrayList<>();
+
+        // 20 Produkte (10 Drinks + 10 Snacks)
+        Object[][] productData = {
+                // DRINKS
+                {"Coca-Cola", "Coca-Cola Company", "Erfrischendes Koffein-Getränk.", "https://upload.wikimedia.org/wikipedia/commons/7/7b/Coca-Cola_bottle_background_removed.png", drinkCat},
+                {"Fanta", "Coca-Cola Company", "Orangenlimonade mit Fruchtgeschmack.", "https://upload.wikimedia.org/wikipedia/commons/f/fd/Fanta_orange_200ml_bottle.jpg", drinkCat},
+                {"Sprite", "Coca-Cola Company", "Zitronenlimonade ohne Koffein.", "https://upload.wikimedia.org/wikipedia/commons/2/27/Sprite_12oz_can.png", drinkCat},
+                {"Lipton Ice Tea", "Lipton", "Pfirsich-Eistee, eiskalt serviert.", "https://upload.wikimedia.org/wikipedia/commons/0/03/Lipton_Ice_Tea_2015.jpg", drinkCat},
+                {"Pepsi", "PepsiCo", "Erfrischendes Cola-Getränk mit Koffein.", "https://upload.wikimedia.org/wikipedia/commons/d/d3/Pepsi_can.png", drinkCat},
+                {"Red Bull", "Red Bull GmbH", "Energydrink für mehr Fokus und Energie.", "https://upload.wikimedia.org/wikipedia/commons/0/02/Red_Bull_can.png", drinkCat},
+                {"Capri-Sun", "Capri-Sun", "Fruchtiger Multivitamin-Saft im Trinkbeutel.", "https://upload.wikimedia.org/wikipedia/commons/4/4e/Capri_Sun_Multivitamin.jpg", drinkCat},
+                {"Vöslauer", "Vöslauer", "Natürliches Mineralwasser – prickelnd.", "https://upload.wikimedia.org/wikipedia/commons/3/37/Voeslauer_water.jpg", drinkCat},
+                {"Mezzo Mix", "Coca-Cola Company", "Kombination aus Cola & Orange.", "https://upload.wikimedia.org/wikipedia/commons/6/60/Mezzo_Mix_Bottle.jpg", drinkCat},
+                {"Orangina", "Suntory", "Kohlensäurehaltige Orangenlimonade mit Fruchtfleisch.", "https://upload.wikimedia.org/wikipedia/commons/f/f2/Orangina_bottle.jpg", drinkCat},
+
+                // SNACKS
+                {"Lay's Paprika", "Lay's", "Knusprige Paprika-Chips.", "https://upload.wikimedia.org/wikipedia/commons/3/3e/Lays_chips_Paprika_flavor.jpg", snackCat},
+                {"Pringles Original", "Pringles", "Der Klassiker unter den Stapelchips.", "https://upload.wikimedia.org/wikipedia/commons/3/39/Pringles_Can.png", snackCat},
+                {"Doritos Nacho Cheese", "Doritos", "Würzige Maischips mit Käse.", "https://upload.wikimedia.org/wikipedia/commons/e/e0/Doritos_Nacho_Cheese.jpg", snackCat},
+                {"Salted Popcorn", "Popz", "Klassisches Popcorn mit Salz.", "https://upload.wikimedia.org/wikipedia/commons/f/f1/Bowl_of_popcorn.jpg", snackCat},
+                {"Oreo Cookies", "Oreo", "Doppelkekse mit Vanillecreme.", "https://upload.wikimedia.org/wikipedia/commons/1/18/Oreo-Two-Cookies.jpg", snackCat},
+                {"Haribo Goldbären", "Haribo", "Fruchtige Gummibärchen.", "https://upload.wikimedia.org/wikipedia/commons/9/90/Haribo_Goldbears.jpg", snackCat},
+                {"Kinder Bueno", "Ferrero", "Haselnuss-Waffelriegel mit Milchcremefüllung.", "https://upload.wikimedia.org/wikipedia/commons/f/fe/Kinder_Bueno.jpg", snackCat},
+                {"Snickers", "Mars", "Erdnuss-Karamell-Schokoriegel.", "https://upload.wikimedia.org/wikipedia/commons/4/45/Snickers-broken.jpg", snackCat},
+                {"Twix", "Mars", "Keksriegel mit Karamell und Schokolade.", "https://upload.wikimedia.org/wikipedia/commons/0/0b/Twix-Pair.jpg", snackCat},
+                {"Milka Alpenmilch", "Milka", "Zarte Schokoladentafel mit Alpenmilch.", "https://upload.wikimedia.org/wikipedia/commons/4/40/Milka_Alpenmilch.jpg", snackCat},
+        };
+
+        for (int i = 0; i < productData.length; i++) {
+            Product product = new Product();
+            product.setName((String) productData[i][0]);
+            product.setMarke((String) productData[i][1]);
+            product.setDescription((String) productData[i][2]);
+            product.setImage((String) productData[i][3]);
+            product.setAltText((String) productData[i][3]);
+            product.setNomNomRating((int) (Math.random() * 5) + 1);
+
+            Set<Categorie> cats = new HashSet<>();
+            cats.add((Categorie) productData[i][4]);
+            product.setLinkedCategories(cats);
+
+            Set<Price> prices = new HashSet<>();
+            prices.add(medium);
+            if (i % 2 == 0) prices.add(small);
+            product.setLinkedPrices(prices);
+
+            // Kommentar zum Produkt
+            Comment comment = new Comment();
+            comment.setAccount(admin);
+            comment.setProduct(product);
+            comment.setText("Mega lecker: " + product.getName());
+            comment.setLikes((int) (Math.random() * 10));
+            comment.setDislikes((int) (Math.random() * 3));
+            commentService.insert(comment);
+
+            Set<Comment> comments = new HashSet<>();
+            comments.add(comment);
+            product.setLinkedComments(comments);
+
+            productService.insert(product);
+            products.add(product);
+        }
+
+        // 5 Combos (Drink + Snack)
+        for (int i = 0; i < 5; i++) {
+            Combo combo = new Combo();
+            combo.setName("Combo " + (i + 1));
+            combo.setDescription("Perfekt für den Snack-Abend.");
+            combo.setAccount(admin);
+            combo.setNomNomRating((int) (Math.random() * 5) + 1);
+            combo.setPrice(BigDecimal.valueOf(4.99 + i));
+
+            Set<Product> comboProducts = new HashSet<>();
+            comboProducts.add(products.get(i));        // Drink
+            comboProducts.add(products.get(i + 10));   // Snack
+            combo.setLinkedProducts(comboProducts);
+
+            Comment comboComment = new Comment();
+            comboComment.setAccount(admin);
+            comboComment.setCombo(combo);
+            comboComment.setText("Diese Kombo haut rein!");
+            comboComment.setLikes((int) (Math.random() * 8));
+            comboComment.setDislikes((int) (Math.random() * 2));
+            commentService.insert(comboComment);
+
+            Set<Comment> comboComments = new HashSet<>();
+            comboComments.add(comboComment);
+            combo.setLinkedComments(comboComments);
+
+            comboService.insert(combo);
+        }
     }
 }
