7a8d49fff1c734bb91e080da6010b7c26114abe6
Kiara BBaldo <zbaldk@bbcag.ch>
2025-04-10 11:23:51 +0200

    ShoppingCartRoute finished

--- frontend/src/components/ShoppingCartFooter.jsx
+++ frontend/src/components/ShoppingCartFooter.jsx
@@ -1,8 +1,6 @@
 import Button from "./Button";
 import styles from "./ShoppingCartFooter.module.css";
 import { useShoppingCart } from "@/lib/shoppingcart";
-<<<<<<< HEAD
-import { useShoppingCart } from "@/lib/shoppingcart";
 
 export default function ShoppingcartFooter({ }) {
     const [products, setProducts] = useShoppingCart()
@@ -10,26 +8,6 @@ export default function ShoppingcartFooter({ }) {
     let totalPrice = 0
 
     for (const product of products) {
-        totalPrice += product.count * Number(product.volumes[0].price)
-    }
-
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.bar}></div>
-            <div className={styles.content}>
-                <p >Total:</p>
-                <div className={styles.footerMargin}>
-                    <p>{`${totalPrice.toFixed(2)}`} CHF</p>
-                    <Button> Zur Kasse </Button>
-                </div>
-=======
-
-export default function ShoppingcartFooter({ }) {
-    const [products, setProducts] = useShoppingCart()
-    
-    let totalPrice = 0
-
-    for(const product of products) {
         totalPrice += product.count * Number(product.volumes[0].price)
     }
 
@@ -44,7 +22,14 @@ export default function ShoppingcartFooter({ }) {
             <div>
                 <p>{totalPrice.toFixed(2)} CHF</p>
                 <Button> Zur Kasse </Button>
->>>>>>> a64520b (ShoppingCartCard optimized)
+                <div className={styles.bar}></div>
+                <div className={styles.content}>
+                    <p >Total:</p>
+                    <div className={styles.footerMargin}>
+                        <p>{`${totalPrice.toFixed(2)}`} CHF</p>
+                        <Button> Zur Kasse </Button>
+                    </div>
+                </div>
             </div>
         </footer>
 
