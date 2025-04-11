bd4c6a365e60dbabb2846b344485b9aaad9aaaf8
Kiara BBaldo <zbaldk@bbcag.ch>
2025-04-11 15:13:31 +0200

    AddToCart finished

--- frontend/src/components/DetailCard.jsx
+++ frontend/src/components/DetailCard.jsx
@@ -59,6 +59,33 @@ export default function DetailCard({ product }) {
     }
 
 
+    function handleAddToCart() {
+        const selectedPriceObj = prices.find(p => p.price == price);
+    
+        const newItem = {
+            id: product.id,
+            name: product.name,
+            image: product.image,
+            price: parseFloat(price),
+            volume: selectedPriceObj?.volume || "",
+            count: parseInt(count),
+        };
+    
+        const cart = JSON.parse(localStorage.getItem("productCart")) || [];
+    
+        const existingItem = cart.find((item) => item.id === newItem.id && item.volume === newItem.volume);
+    
+        if (existingItem) {
+            existingItem.count += newItem.count;
+        } else {
+            cart.push(newItem);
+        }
+    
+        localStorage.setItem("productCart", JSON.stringify(cart));
+        alert("Zum Warenkorb hinzugefügt!");
+    }
+    
+    
     return (
         <>
             {prices ? (
