bc0985f901510531f1720ec7ad0d8472fe30defb
Mikolaj Spychala <zspycm@bbbcag.ch>
2025-04-17 08:06:32 +0200

    sidebar functional

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -2,6 +2,7 @@ import styles from "./SideBar.module.css"
 import "./SideBar.module.css"
 import { Children, useState } from "react";
 import SideBarItem from "./SideBarItem";
+import { Link } from "react-router";
 
 
 export default function SideBar({ sideBarItems }) {
@@ -9,7 +10,7 @@ export default function SideBar({ sideBarItems }) {
     return (
         <>
             <div className={styles.sideBar}>
-                {sideBarItems.map((item) => <SideBarItem title={item.name} items={item.children} />)}
+                {sideBarItems.map((item) => <div><Link className={styles.link}to={item.path}>{item.name}</Link><br></br></div>)}
             </div>
         </>
     )
--- frontend/src/lib/data/sideBarItems.js
+++ frontend/src/lib/data/sideBarItems.js
@@ -1,52 +1,11 @@
 const sideBarItems = [
   {
-    name: "Drinks",
-    children: [
-      {
-        name: "Mit Kohlensäure",
-        children: [
-          { name: "Cola", url: "#" },
-          { name: "Sprite", url: "#" },
-          { name: "Fanta", url: "#" }
-        ]
-      },
-      {
-        name: "Ohne Kohlensäure",
-        children: [
-          { name: "Wasser", url: "#" },
-          { name: "Eistee", url: "#" },
-          { name: "Saft", url: "#" }
-        ]
-      }
-    ]
-  },
-  {
-    name: "Snacks",
-    children: [
-      {
-        name: "Salzig",
-        children: [
-          { name: "Chips", url: "#" },
-          { name: "Nüsse", url: "#" }
-        ]
-      },
-      {
-        name: "Süss",
-        children: [
-          { name: "Schokolade", url: "#" },
-          { name: "Gummibärchen", url: "#" },
-          { name: "Kekse", url: "#" }
-        ]
-      }
-    ]
+    name: "Drinks und Snacks",
+    path: "/products"
   },
   {
     name: "Combos",
-    children: [
-      { name: "Snack + Drink", url: "#" },
-      { name: "Süß + Salzig", url: "#" },
-      { name: "Family Pack", url: "#" }
-    ]
+    path: "/combos"
   }
 ];
 
